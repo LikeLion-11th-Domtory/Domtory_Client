f2fcb60a44f68e6ae0233ee3c977b51b9e5a29b1
Minhyung Cho <dup33333@hufs.ac.kr>
2023-11-23 01:25:02 +0900

    Feat: .env update

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,24 +1,24 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getMessaging, getToken} from "firebase/messaging";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCsNgGOqflGDtxmUp6Kbcbyoh5hUbX9Jfw",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "domtory-c1ec1.firebaseapp.com",
   projectId: "domtory-c1ec1",
   storageBucket: "domtory-c1ec1.appspot.com",
-  messagingSenderId: "8176614070",
-  appId: "1:8176614070:web:f776227696b8df9c2d4417",
-  measurementId: "G-ELF8DS7FSE"
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
 
 async function requestPermission() {
   console.log("권한 요청 중...");
