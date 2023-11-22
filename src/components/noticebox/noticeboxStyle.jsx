eb5330c6712d8a43e4580956069852c10b4d9676
박유진 <qkrdbw@naver.com>
2023-11-22 15:03:34 +0900

    feat: noticebox add

---
+++ src/components/noticebox/noticeboxStyle.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Notice = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 5rem;
+    border: 1px solid #a8a8a8;
+    padding: 0.5rem 0.7rem;
+    margin-bottom: 1rem;
+    
+    img {
+        width: 1rem;
+        height: 1.5rem;
+        margin-right: 1rem;
+    }
+    span {
+        font-size: 1rem;
+    }
+`
--- src/pages/notice/notice.jsx
+++ src/pages/notice/notice.jsx
@@ -7,9 +7,9 @@ import * as HeaderStyles from '../../components/header/headerStyle';
 import GlobalStyle from '../../GlobalStyle';
 import logo from '../../assets/logo.png';
 import back from '../../assets/back.png';
-import mike from '../../assets/mike.png';
 import search from '../../assets/search.png';
 import { faBell, faBars } from '@fortawesome/free-solid-svg-icons';
+import NoticeBox from '../../components/noticebox'
 
 const Back = styled.img`
     padding: 0.4rem 0.3rem 0 0.3rem;
@@ -60,10 +60,7 @@ export default function Notice() {
 
                     {/* 목록 */}
                     {notices.map((notice) => (
-                        <Styles.Notice key={notice.id}>
-                            <img src={mike} />
-                            <span>{notice.title}</span>
-                        </Styles.Notice>
+                        <NoticeBox key={notice.id} notice={notice} />
                     ))}
                 </Styles.Wrapper >
             </Styles.Container >
